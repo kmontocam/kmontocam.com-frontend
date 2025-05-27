88b65571cbf3cd1d14d28136f3801c8bdb9590fc
Kevin Montoya <me@kmontocam.com>
2025-05-27 16:34:39 -0600

    feat: single backend url endpoint for common backend url
    enable ws for common backen

--- vite.config.js
+++ vite.config.js
@@ -108,14 +108,10 @@ export default defineConfig({
          * Proxying backend server through Vite to prevent Access-Control-Allow-Origin issues.
          * Ensure the backend server is running and accessible at the specified URL.
          */
-        target: process.env.PROXY_VITE_BACKEND_URL || 'http://localhost:3000', // eslint-disable-line
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/ws': {
-        target: process.env.PROXY_VITE_BACKEND_WS_URL || 'ws://localhost:3000', // eslint-disable-line
+        target: process.env.PROXY_VITE_BACKEND_URL || 'http://localhost:3000', // eslint-disable-line
         ws: true,
-        rewrite: (path) => path.replace(/^\/ws/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
